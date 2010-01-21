c971b5a02ddce93b45ec7a85f81049401485d2d3
Luis Gonzalez <lgonzalez@ti.com>
2010-01-21 09:31:54 -0600

    android_setenv.sh: add "kw" option to export Klocwork environment variables
    Signed-off-by: Luis Gonzalez <lgonzalez@ti.com>

--- scripts/android_envset.sh
+++ scripts/android_envset.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 usage () {
-	echo "Usage: android_setenv.sh [ gfx ]
+	echo "Usage: android_setenv.sh [ gfx | kw ]
 OPTIONAL:
 	gfx: declare variables required for building GFX DDK
-target_dir:
-	directory path where you want binaries to be stored"
-	exit 64 # command line usage error
+	kw: declare variables required for Klockwork reports"
 }
 
 until [ -z "$1" ]; do
 	case "$1" in 
-	gfx) gfx=1
+		gfx) gfx=1;;
+		kw) kw=1
 	esac
 	shift
 done
@@ -42,7 +41,7 @@ export CROSS_COMPILE=$TOOL_CHAIN_HOME/bin/arm-none-linux-gnueabi-
 echo "TOOL_CHAIN_HOME = $TOOL_CHAIN_HOME"
 echo "CROSS_COMPILE = $CROSS_COMPILE"
 
-#GFX DDK variables:
+# GFX DDK variables:
 if [ -n "$gfx" ]; then
 export CC_PATH=$TOOL_CHAIN_HOME
 export KERNELDIR=$MYDROID/kernel/android-2.6.29
@@ -55,4 +54,17 @@ KERNELDIR = $KERNELDIR
 ANDROID_ROOT = $ANDROID_ROOT
 ANDROID_PRODUCT = $ANDROID_PRODUCT
 DISCIMAGE = $DISCIMAGE"
+fi
+
+# Klockwork variales:
+if [ -n "$kw" ]; then
+export LM_LICENSE_FILE=27005@flames-usa4.sc.ti.com
+export FLEXLM_TIMEOUT=500000
+export KW_HOST=klocwork.dal.design.ti.com
+export KW_PORT=1116
+echo "Your Klocwork variables are:
+LM_LICENSE_FILE=$LM_LICENSE_FILE
+FLEXLM_TIMEOUT=$FLEXLM_TIMEOUT
+KW_HOST=$KW_HOST
+KW_PORT=$KW_PORT"
 fi
